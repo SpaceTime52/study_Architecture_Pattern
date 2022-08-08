b613fabe633661593ed73e9cc0c249980e0ea21c
Bohyeon Kim <love.free.work@gmail.com>
2022-08-08 09:35:12 +0900

    share sample codes for team

--- test/unit/users.test.js
+++ test/unit/users.test.js
@@ -19,15 +19,21 @@ const { a, b, c } = require("../data/user-data-in.js");
 const { x, y, z } = require("../data/user-data-out.js");
 
 // jest.fn() ; - 의존적인 부분을 가짜로 대체하는 mocking function 전달
-// 테스트해볼 각종 메소드 불러오기
-UsersController.create = jest.fn();
-UsersController.find = jest.fn();
+// 테스트 해볼 각종 메소드 나열
+UsersController.signUp = jest.fn();
+UsersController.login = jest.fn();
 
-UsersService.create = jest.fn();
-UsersService.find = jest.fn();
+UsersService.signUp = jest.fn();
+UsersService.getToken = jest.fn();
 
-UserRepository.create = jest.fn();
-UserRepository.find = jest.fn();
+UserRepository.getUserbyNickname = jest.fn();
+UserRepository.getUserbyId = jest.fn();
+UserRepository.getUserbyNicknamePw = jest.fn();
+UserRepository.createUser = jest.fn();
+UserRepository.getAllUsers = jest.fn();
+UserRepository.getAllLikedPosts = jest.fn();
+UserRepository.likePost = jest.fn();
+UserRepository.dislikePost = jest.fn();
 
 // 회원가입 관련 테스트 그룹
 describe("User - Signup-related Test", () => {
@@ -88,4 +94,94 @@ describe("User - Signup-related Test", () => {
   });
 });
 
+describe("User - Login-related Test", () => {
+  //
+  // 회원가입 과정에서 컨트롤러 관련 테스트 그룹
+  describe("User Controllers Layer Test", () => {
+    // 구성
+    test("사용자 컨트롤러 계층은 아래와 같이 구성되어 있어야 한다.", () => {
+      console.log(req);
+      expect(true).toBe(true);
+    });
+
+    // 기능
+    test("사용자 컨트롤러는 아래와 같은 역할을 할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+
+    // 예외처리
+    test("사용자 컨트롤러는 아래와 같은 상황에서도 정상 상태를 유지할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  // 회원가입 과정에서 서비스 관련 테스트 그룹
+  describe("User Services Layer Test", () => {
+    // 구성
+    test("사용자 서비스 계층은 아래와 같이 구성되어 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+
+    // 기능
+    test("사용자 서비스는 아래와 같은 역할을 할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+
+    // 예외처리
+    test("사용자 서비스는 아래와 같은 상황에서도 정상 상태를 유지할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  // 회원가입 과정에서 저장소 관련 테스트 그룹
+  describe("User Repositories Layer Test", () => {
+    // 구성
+    test("사용자 저장소는 아래와 같이 구성되어 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+
+    // 기능
+    test("사용자 저장소는 아래와 같은 역할을 할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+
+    // 예외처리
+    test("사용자 저장소는 아래와 같은 상황에서도 정상 상태를 유지할 수 있어야 한다.", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
 // 회원가입 관련 테스트 그룹
+
+// 시나리오
+// 게시글 - ㅇㅇ 기능 관련 테스트
+// 컨트롤러 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
+
+// 서비스 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
+
+// 저장소 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
+
+// 게시글 - ㅇㅇ 기능 관련 테스트
+// 컨트롤러 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
+
+// 서비스 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
+
+// 저장소 테스트
+// 구조 테스트
+// 기능 테스트
+// 예외 처리 테스트
