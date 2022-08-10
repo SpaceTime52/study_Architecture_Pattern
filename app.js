1addd4ce7f10d01212b909f6a01c398549d33fe7
Bohyeon Kim <love.free.work@gmail.com>
2022-08-10 18:42:06 +0900

    feat swagger related revision

--- app.js
+++ app.js
@@ -14,23 +14,14 @@ app.use(cookieParser());
 app.use(express.json());
 
 // DB에 연결 - sync() 사용으로 DB 모델(./models)의 변화를 감지하여 동기화
-
 const { sequelize } = require("./models");
-// sequelize
-//   .sync({ force: false })
-//   .then(() => {
-//     console.log("데이터베이스 연결 성공");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-// queryInterface: 이 주석은 데이터베이스의 테이블이나 컬럼을 생성하고 이름을 바꾸는 등의 쿼리문을 만드는 데 활용한다.
-// const queryInterface = sequelize.getQueryInterface();
-// queryInterface.dropTable("Tests");
 
 // "/api" path로 연결하는 라우터 연결 (우선 routes/index.js로)
 const apiRouter = require("./routes/index.js");
 app.use("/api", [apiRouter]);
+
+// queryInterface: 이 주석은 데이터베이스의 테이블이나 컬럼을 생성하고 이름을 바꾸는 등의 쿼리문을 만드는 데 활용한다.
+// const queryInterface = sequelize.getQueryInterface();
+// queryInterface.dropTable("Tests");
 
 module.exports = app;
